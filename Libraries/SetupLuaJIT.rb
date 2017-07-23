ac38637695aff809164eb84ced85ee505339c3d4
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-07-23 23:13:00 +0300

    Added some useful code for use in a future LuaJIT library

---
+++ Libraries/SetupLuaJIT.rb
@@ -0,0 +1,15 @@
+# Work in progress module for luajit
+
+abort("unfinished")
+
+# Make sure XCFLAGS+= -DLUAJIT_ENABLE_LUA52COMPAT is uncommented
+outdata = File.read("Makefile").gsub(/#XCFLAGS\+= -DLUAJIT_ENABLE_LUA52COMPAT/,
+                                     "XCFLAGS+= -DLUAJIT_ENABLE_LUA52COMPAT")
+
+File.open("Makefile", 'w') do |out|
+  out << outdata
+end  
+
+runCompiler CompileThreads
+
+onError "Failed to compile luajit" if $?.exitstatus > 0
