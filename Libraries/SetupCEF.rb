9393f1675c3bf515cf5a9620748508b25ce9ce8d
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-10-10 12:48:10 +0300

    Updated CEF available versions

--- Libraries/SetupCEF.rb
+++ Libraries/SetupCEF.rb
@@ -7,35 +7,53 @@ class CEF < ZipAndCmakeDLDep
 
     self.HandleStandardCMakeOptions
 
-    # TODO: do similar version select as an SetupAngelScript.rb
-    @Version = "3.3325.1756.g6d8faa4"
-
     # sha1
     @DLHashType = 3
+
+    if !@Version
+      # @Version = "3.3497.1836.gb472a8d"
+      @Version = "3.3396.1777.g636f29b"
+    end
 
     # Detect platform
     if OS.linux?
       # TODO: also move this to 7z
       @UnZippedName = "cef_binary_#{@Version}_linux64_minimal"
-      @DLHash = "5077b9580862c6304a34fbe334bc05e4ed1fca55"
     elsif OS.windows?
       # Needs to patch visual studio 1913 to the supported ones
       @UnZippedName = "cef_binary_#{@Version}_windows64_minimal"
-      @DownloadURL = "https://boostslair.com/rubysetupsystem/deps/#{@UnZippedName}.7z"
-      @DLHash = "6e5efee25057e8b86c347a0e37f3b034376148d872716cc4771eb5f186b86ad0"
-      @DLHashType = 2
     elsif OS.mac?
       @UnZippedName = "cef_binary_#{@Version}_macosx64_minimal"
-      @DLHash = "96b9d98ee0ee80a1f826588565efc0820070764f"
     else
       onError "Unknown platform for CEF setup"
     end
 
-    if OS.windows?
-      @LocalFileName = @UnZippedName + ".7z"
+    # Version specific hashes
+    case @Version
+    when "3.3497.1836.gb472a8d"
+      if OS.linux?
+        @DLHash = "47c5dd712d7784c7627df53ad2424d7d8f18ed24"
+      elsif OS.windows?
+        @DLHash = "8d1367ee069b0658dde031ff24fc5946e707f17b"
+      elsif OS.mac?
+        @DLHash = "e30bcab7a0688c51e7c97d5c62ab75f9e6adb59b"
+      end
+    when "3.3396.1777.g636f29b"
+      if OS.linux?
+        @DLHash = "fe78f23c64067860a1c963b89e39e6f7ce796074"
+      elsif OS.windows?
+        @DLHash = "2611ca3dc050c41f6dd4ac51ee1b9b3bb7331290"
+      elsif OS.mac?
+        @DLHash = "82e27258a604369e0675fd5d8ed340c21b1029d9"
+      end
     else
+      onError "unknown CEF version: #{@Version}"
+    end
+
+    if !@LocalFileName
       @LocalFileName = @UnZippedName + ".tar.bz2"
     end
+    
     @LocalPath = File.join(CurrentDir, @LocalFileName)
     if !@DownloadURL
       @DownloadURL = "http://opensource.spotify.com/cefbuilds/#{@LocalFileName}"
@@ -51,8 +69,7 @@ class CEF < ZipAndCmakeDLDep
       end
     end
 
-    # For packaging
-    # not that that is needed for this as this is basically precompiled
+    # Version for packaging
     @RepoURL = @DownloadURL
   end
 
@@ -115,12 +132,15 @@ class CEF < ZipAndCmakeDLDep
       installer.addLibrary File.join(@Folder, "Release/", "libcef.so")
       installer.addLibrary File.join(@Folder, "Release/", "libEGL.so")
       installer.addLibrary File.join(@Folder, "Release/", "libGLESv2.so")
-      installer.addLibrary File.join(@Folder, "Release/", "libwidevinecdmadapter.so")
 
       glob = Globber.new "libcef_dll_wrapper.a", File.join(@Folder, "build")
       glob.getResult.each{|f|
         installer.addLibrary f
       }
+
+      FileUtils.mkdir_p File.join(@InstallPath, "bin")
+      copyPreserveSymlinks File.join(@Folder, "Release/", "chrome-sandbox"),
+                           File.join(@InstallPath, "bin")
       
     elsif OS.windows?
       installer.addLibrary File.join(@Folder, "Release/", "libcef.dll")
@@ -128,7 +148,6 @@ class CEF < ZipAndCmakeDLDep
       installer.addLibrary File.join(@Folder, "Release/", "libEGL.dll")
       installer.addLibrary File.join(@Folder, "Release/", "libGLESv2.dll")
       installer.addLibrary File.join(@Folder, "Release/", "d3dcompiler_47.dll")
-      installer.addLibrary File.join(@Folder, "Release/", "widevinecdmadapter.dll")
       installer.addLibrary File.join(@Folder, "Release/", "cef_sandbox.lib")
       installer.addLibrary File.join(@Folder, "Release/", "libcef.lib")
 
@@ -171,7 +190,6 @@ class CEF < ZipAndCmakeDLDep
         "lib/libEGL.dll",
         "lib/libGLESv2.dll",
         "lib/d3dcompiler_47.dll",
-        "lib/widevinecdmadapter.dll",
       ]
     else
       nil
